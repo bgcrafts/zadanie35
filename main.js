782d0c11b9a983004edc5b6d58aac0b00fc2a8d7
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-11 10:49:35 +0100

    zadanie35

---
+++ main.js
@@ -0,0 +1,20 @@
+let f = function (n) {
+    if (n < 100) {
+        return console.log("Liczba nie znajduje się w przedziale");
+    }
+
+    else if (n > 200) {
+
+        return console.log("Liczba nie znajduje się w przedziale");
+
+    }
+
+    else if (n > 100) {
+        return console.log("Liczba znajduje się w przedziale");
+    }
+
+
+    else if (n = 100) {
+        return console.log("Liczba znajduje się w przedziale");
+    }
+}
