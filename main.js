b8acdcf0bc6d0058a6430001191d57493f8f5ad1
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-12 22:02:05 +0100

    poprawione

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 let f = function (n) {
-    if (n < 100) {
+    if (n < 100 || n > 200) {
         return console.log("Liczba nie znajduje się w przedziale");
     }
 
-    else if (n > 200) {
-
-        return console.log("Liczba nie znajduje się w przedziale");
-
-    }
-
-    else if (n > 100) {
-        return console.log("Liczba znajduje się w przedziale");
-    }
-
-
-    else if (n = 100) {
+    else if (n > 100 && n < 200) {
         return console.log("Liczba znajduje się w przedziale");
     }
 }
